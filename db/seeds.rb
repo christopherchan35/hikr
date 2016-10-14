9a1ae33525cd7c3cac06c03f6fa996869c32b0f9
Christopher Chan <christopherchan35@gmail.com>
2016-10-14 12:34:00 -0700

    Add some hikes to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Trail.create(:name => "Stout Grove Trail",
+             :distance => ".6 miles",
+             :time => "30 minutes",
+             :location => "17 W 4th Ave, Crescent City, CA 95531"
+             )
+
+Trail.create(:name => "Lassen Peak Trail",
+             :distance => "5 miles",
+             :time => "3 - 5 hours",
+             :location => "38050 Hwy 36 E, Mineral, CA 96063"
+             )
+
+Trail.create(:name => "Hammond Trail",
+             :distance => "5.5 miles",
+             :time => "")
